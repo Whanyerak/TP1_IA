bac38dd4cd2c5ea4ed76ece8c6cf16d10e50b34d
BlorZ <dupont.david@hotmail.fr>
2017-11-24 11:40:48 +0100

    Initial commit

---
+++ src/tp1/Main.java
@@ -0,0 +1,25 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class Main {
+
+		public static void main(String [] args)
+		{
+			final Taquin t=new Taquin();
+			Scanner sc = new Scanner(System.in);
+			int taille = sc.nextInt();
+			t.initialisation(taille);
+			t.toString(taille);
+			int nbMP = t.nbPiecesMalPlacee(t, taille);
+			System.out.println("\n"+ nbMP + " pièces sont mal placées");
+			sc.close();
+			
+			//résolution taquin
+			//Interdire les états répétés
+			//faire une fonction déplacer=> faire un tableau de possibilité
+			//pour retenir les possibilités
+			//faire un arbre de recherche pour trouver la solution
+			//comparer a chaque boucle a la solution finale
+		}
+}
---
+++ src/tp1/Taquin.java
@@ -0,0 +1,76 @@
+package tp1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Taquin {
+	public int taquin[][] = new int[3][3];
+	
+	
+	public Taquin() {
+	}
+	
+	public void initialisation(int taille) {
+		int i,j,k, l;
+		List<Integer> list = new ArrayList<>();
+		for(i=0; i<taille*taille; i++) {
+			list.add(i);
+		}
+		for (i=0;i<taille;i++) {
+			for(j=0;j<taille;j++) {
+				k=(int) (Math.random()*list.size());
+				l=list.get(k);
+				taquin[i][j]=l;
+				list.remove(k);
+			}
+		}
+	}
+	
+	public String toString(int taille) {
+		int i,j;
+		for (i=0;i<taille;i++) {
+			System.out.println("");
+			for(j=0;j<taille;j++) {
+				System.out.print("|");
+				System.out.print(taquin[i][j]);
+			}
+			System.out.print("|");
+		}
+		return "";
+	}
+	
+	public int nbPiecesMalPlacee(Taquin taquin, int taille) {
+		int nbPM=0, i , j , k;
+		for (i=0;i<taille;i++) {
+			for(j=0;j<taille;j++) {
+				switch(i) {
+					case 0 : {
+						k = 0;
+						break;
+					}
+					case 1 : {
+						k = 3;
+						break; 
+					}
+					case 2 : {
+						k = 6;
+						break;
+					}
+					default : k=0;
+				}
+				if(this.taquin[i][j] != j+k)
+					nbPM++;
+			}
+		}
+		return nbPM;
+	}
+	
+	public int distanceManhattan(Taquin taquin, int taille) {
+		int dM = 0;
+		for (int i=0;i<taille;i++) {
+			for(int j=0;j<taille;j++) {
+			}
+		}
+		return dM;
+	}
+}
