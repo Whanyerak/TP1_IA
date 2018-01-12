6348773a5baf9392b3e029fc836b6a4f75661a2a
BlorZ <dupont.david21@hotmail.fr>
2018-01-12 11:15:59 +0100

    push (presque) final

---
+++ src/tp1/Taille.java
@@ -0,0 +1,8 @@
+//TP Réalisé par Valentin MORIN et surtout par Dahavid DUPONT
+// Etudiants en MASTER TNSI à l'UVHC
+
+package tp1;
+
+public class Taille {
+	public static int N;
+}
--- src/tp1/Taquin.java
+++ src/tp1/Taquin.java
@@ -1,264 +1,449 @@
+//TP Réalisé par Valentin MORIN et surtout par Dahavid DUPONT
+// Etudiants en MASTER TNSI à l'UVHC
+
 package tp1;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Taquin {
-	private  static final Logger LOGGER = Logger.getLogger(Taquin.class.getName());
-	int taille = 3;
-	public int[][] taquin = {{1,5,6},
-							{2,8,0},
-							{3,7,4}};
-		
-	public Taquin() {
-		super();
-	}
-	
-	public int getTaille() {
-		return taille;
-	}
+	public int taille = 3;
+	public int[][] tab = new int[taille][taille];
+	public int mp; // Pièces mal positionnées
+	public int manhattan; // Distance de Manhattan: somme des distances de chaque pièce à sa position but
+	public int g; //nombre de coups
+	public Integer f; // g+manhattan
+	public int visite; //A-t-il était visité?
+	public Taquin parent;
 
-	public void setTaille(int taille) {
-		this.taille = taille;
-	}
-
-	public int[][] getTaquin() {
-		return taquin;
-	}
-
-	public void setTaquin(int[][] taquin) {
-		this.taquin = taquin;
-	}
-
-	public void initialisation() {
-		int i;
-		int j;
-		int k;
-		int l;
-		List<Integer> list = new ArrayList<>();
-		for(i=0; i<this.taille*this.taille; i++) {
-			list.add(i);
-		}
-		for (i=0;i<this.taille;i++) {
-			for(j=0;j<this.taille;j++) {
-				k=(int) (Math.random()*list.size());
-				l=list.get(k);
-				taquin[i][j]=l;
-				list.remove(k);
+	public void initBut()
+	{
+		int i, j, v=0;
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				this.tab[i][j] = v;
+				v++;
 			}
 		}
+		this.mp = 0;
+		this.manhattan = 0;
+		this.g = 0;
+		this.f = 0;
+		this.parent = null;
 	}
 	
-	public void initialisationEtatFinal() {
-		int i;
-		int j;
-		int k=0;
-		for (i=0;i<this.taille;i++) {
-			for(j=0;j<this.taille;j++) {
-				taquin[i][j]=k;
-				k++;
-			}
-		}
-	}
-	
-	public String toString() {
-		int i;
-		int j;
-		for (i=0;i<this.taille;i++) {
-			System.out.println("");
-			for(j=0;j<this.taille;j++) {
-				System.out.print("|");
-				System.out.print(taquin[i][j]);
-			}
-			System.out.print("|");
-		}
-		System.out.println("");
-		return null;
-	}
-	
-	public int nbPiecesMalPlacee() {
-		int nbPM=0;
-		int i;
-		int j;
-		int k;
-		for (i=0;i<this.taille;i++) {
-			for(j=0;j<taille;j++) {
-				switch(i) {
-					case 0 : 
-						k = 0;
-						break;
-					case 1 : 
-						k = 3;
-						break; 
-					case 2 : 
-						k = 6;
-						break;
-					default : k=0;
-				}
-				if(this.taquin[i][j] != j+k)
-					nbPM++;
-			}
-		}
-		return nbPM;
-	}
-	
-	public int distanceManhattan() {
-		int dM = 0;
-		int x;
-		int y;
-		for (int i=0;i<this.taille;i++) {
-			for(int j=0;j<this.taille;j++) {
-				x= trouveX(this.taquin[i][j]);
-				y = trouveY(this.taquin[i][j]);
-				dM+=calculX(x, i);
-				dM+=calculY(y, j);
-			}
-		}
-		return dM;
-	}
-	
-	public int distanceManhattanUnique(int i, int j) {
-		int dM = 0;
-		int x;
-		int y;
-		x= trouveX(this.taquin[i][j]);
-		y = trouveY(this.taquin[i][j]);
-		dM+=calculX(x, i);
-		dM+=calculY(y, j);
-		
-		return dM;
-	}
+	public void init()
+	{
+		// v = tableau de valeurs
+		// x = Tampon pour l'indice
+		int i, j;
+		ArrayList<Integer> v = new ArrayList<Integer>();
+		int x; 
 
-	public int calculX(int x, int x1) {
-		int dM = 0;
-		if(x1>x) {
-			dM += x1-x;
-			return dM;
+		//Initialisation
+		for(i=0; i<(taille*taille); i++)
+		{
+			v.add(i); 
 		}
-		if(x1<x) {
-			dM += x-x1;
-			return dM;
+
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				x = (int) Math.floor((Math.random() * (v.size()-1)));
+				tab[i][j] = v.get(x);
+				v.remove(x);
+			}
 		}
-		return dM;
+		this.parent = null;
 	}
 	
-	public int calculY(int y, int y1) {
-		int dM= 0;
-		if(y1>y) {
-			dM = y1-y;
-			return dM;
+	void nbMalPositionnees()
+	{
+		int i, j, x = 0;
+		Taquin but = new Taquin();
+		but.initBut();
+
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				if(this.tab[i][j] != but.tab[i][j])
+					x++;
+			}
 		}
-		if(y1<y) {
-			dM = y-y1;
-			return dM;
-		}
-		return dM;
+		this.mp = x;
 	}
 	
-	public int trouveX(int valeur) {
-		switch(valeur) {
-			case 0 : return 0;
-			case 1 : return 0;
-			case 2 : return 0;
-			case 3 : return 1;
-			case 4 : return 1;
-			case 5 : return 1;
-			case 6 : return 2;
-			case 7 : return 2;
-			case 8 : return 2;
-			default : 
-				LOGGER.warning("Warning : Il y a eu un incident avec les valeurs, X= -1");
-				return -1;
-		}
-	}
-	
-	public int trouveY(int valeur) {
-		switch(valeur) {
-			case 0 : return 0;
-			case 1 : return 1;
-			case 2 : return 2;
-			case 3 : return 0;
-			case 4 : return 1;
-			case 5 : return 2;
-			case 6 : return 0;
-			case 7 : return 1;
-			case 8 : return 2;
-			default : 
-				LOGGER.warning("Warning : Il y a eu un incident avec les valeurs, Y= -1");
-				return -1;
-		}
-	}
-	
-	public boolean equals(Object o) {
-		Taquin other = (Taquin) o;
-		if(getTaille() != other.taille) {
-			return false;
-		}
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				if(this.taquin[i][j]!= other.taquin[i][j]) {
-					return false;
+	int[] chercherPosition(int valeur)
+	{
+		int i,j;
+		int[] pos = {0,0};
+		for (i = 0; i < taille; i++)
+		{
+			for (j = 0; j < taille; j++)
+			{
+				if(this.tab[i][j] == valeur)
+				{
+					pos[0] = i;
+					pos[1] = j;
+					return pos;
 				}
 			}
 		}
-		return true;
+		return pos;
 	}
 	
-	public void deplacer(List<int[][]> etats, int[] coord0, int[] coordPieceADeplacer) {
-		int tmp = 0;
-		tmp = this.taquin[coord0[0]][coord0[1]];
-		this.taquin[coord0[0]][coord0[1]] = this.taquin[coordPieceADeplacer[0]][coordPieceADeplacer[1]];
-		this.taquin[coordPieceADeplacer[0]][coordPieceADeplacer[1]] = tmp;
-		
-		etats.add(taquin);
-	}
-	
-	/**
-	 *  trouve les coordonnées de la case 0
-	 * @param x
-	 * @param y
-	 * @throws Exception 
-	 */
-	public int[] trouve0() throws Exception{
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				if(taquin[i][j]==0) {
-					return new int[]{i,j};
+	void manhattan()
+	{
+		int i,j,m = 0;
+		int[] pos = null;
+		int x,y;
+
+		Taquin but = new Taquin();
+		but.initBut();
+
+		// Pour chaque pièce
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				// Si ce n'est pas la bonne pièce
+				if(this.tab[i][j] != but.tab[i][j])
+				{
+					//On cherche la position dans l'état but
+					pos = chercherPosition(but.tab[i][j]);
+					x = Math.abs(i - pos[0]);
+					y = Math.abs(j - pos[1]);
+					m += x;
+					m += y;
 				}
 			}
+
 		}
-		throw new Exception("il n'y a pas de case 0 dans le taquin");
+
+		this.manhattan = m;
 	}
 	
-	public int[] trouvePieceADeplacer() throws Exception{
-		int k = 999;// on initialise à très grand pour etre sur de récup la premiere valeur
-		int tmp = 0;
-		int[] coordonnees = trouve0();
-		int[] coordPieceAdeplacer = {999,999};
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				//on recherche la piece avec le moins de distance de manhattan autour du 0
-				if(((i == coordonnees[0]+1 || i == coordonnees[0]-1)&& j == coordonnees[1]) || ((j==coordonnees[1]+1 || j == coordonnees[1]-1)&& i == coordonnees[0])) {
-					tmp = distanceManhattanUnique(i, j);
-					if(tmp < k) {
-						k = tmp;
-						coordPieceAdeplacer[0] = i;
-						coordPieceAdeplacer[1] = j;
+	void deplacementGauche()
+	{
+		int x, y;
+		for(x=0; x<taille; x++)
+		{
+			for(y=0; y<taille; y++)
+			{
+				if(this.tab[x][y] == 0)
+				{
+					if(y == (taille-1))
+					{
+						return;
 					}
+					this.tab[x][y] = this.tab[x][y+1];
+					this.tab[x][y+1] = 0;
+					return;
 				}
 			}
 		}
-		return coordPieceAdeplacer;
 	}
-	
-	public boolean verifieEtat(List<int[][]> etats) {
-		for(int[][] etat : etats) {
-			if(this.taquin.equals(etat)) {
-				return false;
+
+	void deplacementDroite()
+	{
+		int x, y;
+		for(x=0; x<taille; x++)
+		{
+			for(y=0; y<taille; y++)
+			{
+				if(this.tab[x][y] == 0)
+				{
+					if(y == 0)
+					{
+						return;
+					}
+					this.tab[x][y] = this.tab[x][y-1];
+					this.tab[x][y-1] = 0;
+					return;
+				}
 			}
 		}
-		return true;
+	}
+
+	void deplacementBas()
+	{
+		int x,y;
+		for(x=0; x<taille; x++)
+		{
+			for(y=0; y<taille; y++)
+			{
+				if(this.tab[x][y] == 0)
+				{
+					if(x == 0)
+					{
+						return;
+					}
+					this.tab[x][y] = this.tab[x-1][y];
+					this.tab[x-1][y] = 0;
+					return;
+				}
+			}
+		}
+	}
+
+	void deplacementHaut()
+	{
+		int x,y;
+		for(x=0; x<taille; x++)
+		{
+			for(y=0; y<taille; y++)
+			{
+				if(this.tab[x][y] == 0)
+				{
+					if(x == (taille-1))
+					{
+						return;
+					}
+					this.tab[x][y] = this.tab[x+1][y];
+					this.tab[x+1][y] = 0;
+					return;
+				}
+			}
+		}
 	}
 	
+	void copie(Taquin nouveau)
+	{
+		int i, j;
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				nouveau.tab[i][j] = this.tab[i][j];
+			}
+		}
+		nouveau.mp = this.mp;
+		nouveau.manhattan = this.manhattan;
+		nouveau.g = this.g;
+		nouveau.f = this.f;
+		nouveau.parent = this.parent;
+	}
+	
+	//Retourne 1 si les deux taquins sont égaux
+	int compare(Taquin nouveau)
+	{
+		int i, j;
+		for(i=0; i<taille; i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				if(nouveau.tab[i][j] != this.tab[i][j])
+					return 0;
+			}
+		}
+		if(nouveau.mp != this.mp)
+			return 0;
+		if(nouveau.manhattan != this.manhattan)
+			return 0;
+		if(nouveau.g != this.g)
+			return 0;
+		if(nouveau.f != this.f)
+			return 0;
+		return 1;
+	}
+	
+	public void afficherTaquin()
+	{
+		int i, j, k;
+		System.out.print("+----+");
+		for(i=0; i<((taille)-1); i++)
+		{
+			System.out.print("----+");
+		}
+		System.out.println("\n");
+		for(i=0; i<(taille); i++)
+		{
+			for(j=0; j<taille; j++)
+			{
+				String result = String.format("| %2d ", this.tab[i][j]);
+				System.out.print(result);
+			}
+			System.out.print("|\n+----+");
+			for(k=0; k<(taille)-1; k++)
+				System.out.print("----+");
+			System.out.println("\n");
+		}
+		this.nbMalPositionnees();
+		System.out.println("Nombre de pièces mal positionnées : " + this.mp);
+		this.manhattan();
+		System.out.print("Distance de Manhattan : " + this.manhattan + "\n");
+		System.out.print("f : " + this.f + "\n");
+	}
+	
+//	static Taquin getMin(ArrayList<Taquin> a)
+//	{
+//		int min = 999;
+//		Taquin ta = new Taquin();
+//		for(Taquin t : a)
+//		{
+//			if(t.f < min)
+//			{
+//				min = t.f;
+//				t.copie(ta);
+//			}
+//		}
+//		return ta;
+//	}
+	
+	public void aEtoile()
+	{
+		ArrayList<Taquin> ouverts = new ArrayList<Taquin>();
+		ArrayList<Taquin> fermes = new ArrayList<Taquin>();
+		ArrayList<Taquin> fils = new ArrayList<Taquin>();
+		final long debut = System.currentTimeMillis();
+		final long fin;
+		
+		ouverts.add(this);
+		
+		while(ouverts.size() > 0)
+		{
+			Taquin courant = new Taquin();
+			Taquin haut = new Taquin();
+			Taquin bas = new Taquin();
+			Taquin gauche = new Taquin();
+			Taquin droite = new Taquin();
+			
+			//System.out.println("ouverts : " + ouverts.size());
+			//System.out.println("fermes : " + fermes.size());
+			
+			//Tri la liste
+			Collections.sort(ouverts, new Comparator<Taquin>()
+			{
+			    public int compare(Taquin o1, Taquin o2)
+			    {
+			         return o1.f.compareTo(o2.f);
+			    }
+			});
+			
+			//On sélectionne le taquin avec la plus petite heuristique
+			courant = ouverts.get(0);
+			//courant.afficherTaquin();
+			
+			fermes.add(courant);
+			ouverts.remove(courant);
+			
+			//Est-ce la solution ?
+			if(courant.manhattan == 0)
+			{
+				fin = System.currentTimeMillis();
+				System.out.println("Solution trouvée en " + (fin - debut) + "ms");
+				courant.afficherTaquin();
+				return;
+			}
+
+			courant.copie(haut);
+			courant.copie(bas);
+			courant.copie(gauche);
+			courant.copie(droite);
+				
+			haut.deplacementHaut();
+			haut.nbMalPositionnees();
+			haut.manhattan();
+			//si courant != haut -> mouvement effectué
+			if(haut.compare(courant) == 0 && !fermes.contains(haut))
+			{
+				haut.g = haut.g + 1;
+				haut.f = haut.g + haut.manhattan;
+				haut.parent = courant;
+				fils.add(haut);
+			}
+
+			droite.deplacementDroite();
+			droite.nbMalPositionnees();
+			droite.manhattan();
+			if(droite.compare(courant) == 0 && !fermes.contains(haut))
+			{
+				droite.g = droite.g + 1;
+				droite.f = droite.g + droite.manhattan;
+				droite.parent = courant;
+				fils.add(droite);
+			}
+
+			bas.deplacementBas();
+			bas.nbMalPositionnees();
+			bas.manhattan();
+			if(bas.compare(courant) == 0 && !fermes.contains(haut))
+			{
+				bas.g = bas.g + 1;
+				bas.f = bas.g + bas.manhattan;
+				bas.parent = courant;
+				fils.add(bas);
+			}
+
+			gauche.deplacementGauche();
+			gauche.nbMalPositionnees();
+			gauche.manhattan();
+			if(gauche.compare(courant) == 0 && !fermes.contains(haut))
+			{
+				gauche.g = gauche.g + 1;
+				gauche.f = gauche.g + gauche.manhattan;
+				gauche.parent = courant;
+				fils.add(gauche);
+			}
+			
+			courant.initBut();
+			
+			//System.out.println("Fils = " + fils.size());
+			
+			for (Taquin noeud : fils) 
+			{
+				//Est-ce la solution ?
+//				if(noeud.manhattan == 0)
+//				{
+//					fin = System.currentTimeMillis();
+//					System.out.println("Solution trouvée en " + (fin - debut) + "ms");
+//					courant.afficherTaquin();
+//					return;
+//				}
+//				else
+//				{
+//					System.out.println("action");
+//					if (!ouverts.contains(noeud) && !fermes.contains(noeud)) 
+//					{
+//						ouverts.add(noeud);
+//					}
+//					if ((ouverts.contains(noeud)) && (ouverts.get(ouverts.indexOf(noeud)).g < noeud.g))
+//					{
+//						System.out.println("action2");
+//						ouverts.remove(ouverts.indexOf(noeud));
+//						ouverts.add(noeud);
+//					}
+//					if ((fermes.contains(noeud)) && (fermes.get(fermes.indexOf(noeud)).g < noeud.g))
+//					{
+//						System.out.println("action3");
+//						fermes.remove(fermes.indexOf(noeud));
+//						ouverts.add(noeud);
+//					}
+//				}
+				ouverts.add(noeud);
+			}
+			
+			fils.clear();
+			
+		}//while
+	}
+	
+	public static void main (String args[])
+	{
+		Taquin t = new Taquin();
+		t.init();
+		t.nbMalPositionnees();
+		t.manhattan();
+		t.g = 0;
+		t.f = t.manhattan;
+		t.afficherTaquin();
+		
+		
+		t.aEtoile();
+	}
 }
